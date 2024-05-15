09d4b5fdd9cef380f44d6c3dc5289632f0cc4bc1
ambuj9890 <ainadcab@gmail.com>
2024-05-15 10:56:28 +0530

    success

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\admin\HomeController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('login',[HomeController::class,'login'])->name('admin.login');
